5478929f3b1b740191dd29c06be6c51e85f86aae
Chameison <chameison.acre@gmail.com>
2022-10-18 17:02:20 -0500

    calculadora simples estilizada parcialmente

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function displaynum(n1) {
-    calculadora.text1.value = calculadora.text1.value + n1;
+
+    if(n1 == '/'){
+        funcao1();
+        calculadora.text1.value = calculadora.text1.value + n1;
+    }else{
+        calculadora.text1.value = calculadora.text1.value + n1;
+    }
+}
+function funcao1()
+{
+alert("Lembre-se, qualquer numero divido por 0 leva ao infinito!");
 }
